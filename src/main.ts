caed76f5f6dea777e9fa1697aedcfc41a1a6e081
ghislaingenay <ghislain.gny@gmail.com>
2025-08-10 20:55:39 +0700

    feat: add haunted scene with lighting and camera setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import shadow from "./shadow";
+import haunted from "./haunted";
 import tick from "./light";
 
 const learning = {
   light: tick,
   shadow: shadow,
+  haunted: haunted,
 };
-learning.shadow();
+learning.haunted();

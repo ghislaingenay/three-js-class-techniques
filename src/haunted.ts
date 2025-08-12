7606701d1a61d3cbc627c7adf35e1dba1c0fa13a
ghislaingenay <ghislain.gny@gmail.com>
2025-08-12 10:22:24 +0700

    feat[haunted]: add ground

--- src/haunted.ts
+++ src/haunted.ts
@@ -21,6 +21,14 @@ const sphere = new THREE.Mesh(
 );
 scene.add(sphere);
 
+// Floor
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial()
+);
+floor.rotation.x = -Math.PI * 0.5; // Rotate the floor to be horizontal
+scene.add(floor);
+
 /**
  * Lights
  */
--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,5 @@ const learning = {
   shadow: shadow,
   haunted: haunted,
 };
+
 learning.haunted();

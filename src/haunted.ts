f027e3f25129a721a84882df48f622d050215fc1
ghislaingenay <ghislain.gny@gmail.com>
2025-08-12 10:42:42 +0700

    feat: add bushes and graves around the house

--- src/haunted.ts
+++ src/haunted.ts
@@ -53,6 +53,50 @@ door.position.y = 1 + 0.01;
 door.position.z = 2 + 0.01; // Position the door in front of the house
 house.add(door);
 
+// Bushes
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+const bushMaterial = new THREE.MeshStandardMaterial();
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush1.scale.set(0.5, 0.5, 0.5);
+bush1.position.set(1, 0.5, 2.5);
+house.add(bush1);
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush2.scale.set(0.65, 0.65, 0.65);
+bush2.position.set(-1.8, 0.1, 2.1);
+house.add(bush2);
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush3.scale.set(0.4, 0.4, 0.4);
+bush3.position.set(1.8, 0.3, 2.3);
+house.add(bush3);
+
+// Graves
+const graveGeometry = new THREE.BoxGeometry(0.5, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial();
+
+const graves = new THREE.Group();
+scene.add(graves);
+
+for (let i = 0; i < 30; i++) {
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+  // Place around the house => Trigonometric functions
+  const angle = Math.random() * Math.PI * 2; // Random angle
+  const radius = 4 + Math.random() * 5; // Random distance from the house
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
+  const y = Math.random() * 0.4; // Random height variation
+  grave.position.set(x, y, z);
+  const ROTATION_FACTOR = 0.4;
+  grave.rotation.set(
+    ROTATION_FACTOR * (Math.random() - 0.5),
+    ROTATION_FACTOR * (Math.random() - 0.5),
+    ROTATION_FACTOR * (Math.random() - 0.5)
+  ); // Random rotation
+  graves.add(grave);
+}
+
 /**
  * Lights
  */

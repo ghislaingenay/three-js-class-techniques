5e67c466830c9b9c0fedcb8ff412eb663908be92
ghislaingenay <ghislain.gny@gmail.com>
2025-08-12 12:13:57 +0700

    feat: add particle system implementation with basic scene setup and controls

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import shadow from "./shadow";
 import haunted from "./haunted";
 import tick from "./light";
+import particle from "./particle";
 
 const learning = {
   light: tick,
   shadow: shadow,
   haunted: haunted,
+  particle: particle,
 };
 
 learning.haunted();
